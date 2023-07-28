40aeb0f176eb4645c5e02b8fb9a831ab55eced20
Matheus Lucas <matheus.lucas1105@gmail.com>
2023-07-28 10:23:43 -0300

    name change

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8cf54b4e17cf47d8"; }
+/******/ 		__webpack_require__.h = function() { return "90665a4ffea302fa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="bg-gray-900 text-white p-4 text-center flex flex-col items-center">
       
-        <h1 className="text-4xl font-bold">Matheus </h1>
+        <h1 className="text-4xl font-bold">Matheus Lucas</h1>
         <h2 className="text-xl"> Desenvolvedor Full Stack JavaScript | React | Node.js</h2>
    
 
